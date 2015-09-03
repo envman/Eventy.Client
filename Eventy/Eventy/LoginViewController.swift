c8da7d2d6eb8cb35cce7ff443c5302b9f98a4efe
HarryGoodwin <harrygoodwin0@gmail.com>
2015-09-03 21:22:45 +0100

    Added new cancel and register buttons and added methods for changing screen state to register/login

--- Eventy/Eventy/LoginViewController.swift
+++ Eventy/Eventy/LoginViewController.swift
@@ -12,8 +12,11 @@ class LoginViewController : UIViewController
 {
 	@IBOutlet weak var titleLabel: UILabel!
 	@IBOutlet weak var usernameTextField: UITextField!
+	@IBOutlet weak var emailTextField: UITextField!
 	@IBOutlet weak var passwordTextField: UITextField!
 	@IBOutlet weak var loginButton: UIButton!
+	@IBOutlet weak var registerButton: UIButton!
+	@IBOutlet weak var cancelButton: UIButton!
 	
 	override func viewDidLoad()
 	{
@@ -30,36 +33,70 @@ class LoginViewController : UIViewController
 	
 	override func viewDidAppear(animated: Bool)
 	{
-		animateViews()
+		setViewForLogin()
 	}
 	
-	func animateViews()
+	func setViewForLogin()
 	{
 		appearAnimationForView(titleLabel)
-		appearAnimationForView(usernameTextField)
+		appearAnimationForView(emailTextField)
 		appearAnimationForView(passwordTextField)
 		
-		appearAnimationForLogin()
+		appearAnimationForButton(loginButton)
+		appearAnimationForButton(registerButton)
+		
+		usernameTextField.hidden = true;
+		cancelButton.hidden = true
+		registerButton.hidden = false
+		
+		loginButton.setTitle("Login", forState:UIControlState.Normal)
 	}
 	
 	func appearAnimationForView(view: UIView)
 	{
 		let originalViewHeight = view.frame.size.height
 		view.frame.size.height = 0
-		
-		UIView.animateWithDuration(0.5, animations: {
+
+		UIView.animateKeyframesWithDuration(0.5, delay: 0.3, options: [], animations: {
 			view.frame.size.height = originalViewHeight
-		})
+			}, completion: nil)
 	}
 	
-	func appearAnimationForLogin()
+	func appearAnimationForButton(view: UIView)
 	{
-		let originalButtonPosition = loginButton.center
-		loginButton.center = CGPointMake(originalButtonPosition.x, 1000)
+		let originalButtonPosition = view.center
+		view.center = CGPointMake(originalButtonPosition.x, 1000)
 		
-		UIView.animateWithDuration(0.6, animations: {
-			self.loginButton.center = originalButtonPosition
+		UIView.animateKeyframesWithDuration(0.5, delay: 0.3, options: [], animations: {
+			view.center = originalButtonPosition
+		}, completion: nil)
+	}
+	@IBAction func registerButtonPressed(sender: AnyObject)
+	{
+		setViewForRegister()
+	}
+	
+	func setViewForRegister()
+	{
+		loginButton.setTitle("Register", forState:UIControlState.Normal)
+		
+		cancelButton.hidden = false
+		registerButton.hidden = true
+		usernameTextField.hidden = false
+		usernameTextField.alpha = 0
+		
+		let originalButtonPosition = usernameTextField.center
+		usernameTextField.center = emailTextField.center
+		
+		UIView.animateWithDuration(0.4, animations: {
+			self.usernameTextField.center = originalButtonPosition
+			self.usernameTextField.alpha = 1
 		})
+	}
+
+	@IBAction func cancelButtonPressed(sender: AnyObject)
+	{
+		setViewForLogin()
 	}
 }
 
